409aab9aa1e42b3de750a760de3f4dcbb07f6bec
Will <yh2689@nyu.edu>
2021-12-05 16:00:31 +0800

    2.0 test

---
+++ train_redq_sac_exp2_load.py
@@ -0,0 +1,16 @@
+import gym
+import numpy as np
+import torch
+import time
+import sys
+from redq_modified.algos.redq_sac import REDQSACAgent
+from redq_modified.algos.core import mbpo_epoches, test_agent
+from redq_modified.utils.run_utils import setup_logger_kwargs
+from redq_modified.utils.bias_utils import log_bias_evaluation
+from redq_modified.utils.logx import EpochLogger
+
+import joblib
+
+if __name__ == '__main__':
+    agent = joblib.load('./train/mr.agent')
+    print(agent)
